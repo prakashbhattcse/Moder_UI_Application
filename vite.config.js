5e256556fb233cb6617cabc9066ab277ac62bcb7
Prakash Chandra Bhatt <77747508+prakashbhattcse@users.noreply.github.com>
2023-08-21 06:41:14 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/modern bank application"
+  base:"/Moder_UI_Application"
 })
